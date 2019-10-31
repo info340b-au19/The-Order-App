5365f6a44183668a8d7ee39e6a6b71fd045aa94e
Eric Ng <ehng@uw.edu>
2019-10-31 16:46:15 -0700

    some errors still

--- js/index.js
+++ js/index.js
@@ -1,3 +1,21 @@
 'use strict';
 
-$("div");
+$(function() {
+  var tabs = $(".sbbutton");
+  var content = $(".content");
+  var contentTabs = [$("#restaurants"), $("#home"), $("#menu"), $("#service"), $("#myorder")];
+  console.log(content);
+  
+  Array.prototype.forEach.call(tabs, function(element, index) {
+    element.addEventListener("click", changeTabs);
+  });
+  
+  function changeTabs() {
+    $(".current").removeClass("current");
+    $(this).addClass("current");
+    for (var i = 0; i < content.length; i++) {
+      contentTabs[i].find("div")
+    }
+    
+  }
+});

1a7b6b01f038ae86eafdf44114ec2902151eb799
Eric Ng <ehng@uw.edu>
2019-11-04 03:26:49 -0800

    added json and review section

--- js/index.js
+++ js/index.js
@@ -33,32 +33,51 @@ $(function () {
 
   // ------------------------------------------
 
+  // RESTAURANT CONTENT FUNCTIONALITY
+
   $("#mcd").click(homeScreen);
 
   function homeScreen() {
-    alert("hello");
+    contentTabs[0].addClass("hide");
+    $(".current").removeClass("current");
+    contentTabs[1].removeClass("hide");
+    $("#sbhome").addClass("current");
   }
 
 
-  // HOME CONTENT
+  // HOME CONTENT FUNCTIONALITY
 
   // QUEUE BUTTON
 
   $("#queue").click(queue);
 
   function queue() {
-    var input = $("#group-number");
-    if (input.val() && Math.floor(input.val()) == input.val() && $.isNumeric(input.val())) {
-      var q = $(this);
-      var qnumber = $("#queue-number");
-      if (q.hasClass("queue-active")) {
-        q.removeClass("queue-active");
-        qnumber.html("9");
-        input.prop("disabled", false);
+    var q = $(this);
+    var qnumber = $("#queue-number");
+    if (q.hasClass("delivery")) {
+      // RESET DINE-IN BUTTONS
+
+      if (q.hasClass("queue-dactive")) {
+        q.removeClass("queue-dactive");
+        qnumber.html("3");
+
+
       } else {
-        $(this).addClass("queue-active");
-        qnumber.html("10");
-        input.prop("disabled", true);
+        q.addClass("queue-dactive");
+        qnumber.html("4");
+      }
+    } else {
+      var input = $("#group-number");
+      if (input.val() && Math.floor(input.val()) == input.val() && $.isNumeric(input.val())) {
+        if (q.hasClass("queue-active")) {
+          q.removeClass("queue-active");
+          qnumber.html("9");
+          input.prop("disabled", false);
+        } else {
+          q.addClass("queue-active");
+          qnumber.html("10");
+          input.prop("disabled", true);
+        }
       }
     }
   }
@@ -66,7 +85,7 @@ $(function () {
   var mintes = 15 * 60 * 1000;
 
   var countdown = function () {
-    $("#queue-wait").each(function() {
+    $("#queue-wait").each(function () {
       var count = parseInt($(this).html());
       if (count !== 0) {
         $(this).html(count - 1);
@@ -83,7 +102,15 @@ $(function () {
 
   function delivery() {
     var deliveryButton = $("#home-delivery");
-    if(deliveryButton.hasClass("delivery")) {
+
+    // RESET SCREENS AND BUTTON
+    $("#queue").removeClass("queue-active");
+    $("#queue-number").html("9");
+    $("#group-number").prop("disabled", false);
+    $("#group-number").val("");
+    $("#queue").removeClass("queue-dactive");
+
+    if (deliveryButton.hasClass("delivery")) {
       deliveryButton.removeClass("delivery");
       deliveryButton.addClass("dhover");
       deliveryButton.html("DINE IN");
@@ -129,4 +156,100 @@ $(function () {
   }
 
   // ------------------------------------------
+
+  // MENU FUNCTIONALITY
+
+  // MENU - REVIEWS
+  $("#rbutton").click(review);
+
+  function review() {
+    $("#overlay").removeClass("hidden");
+    fetch("../review.json")
+      .then(function (response) {
+        var dataPromise = response.json();
+        return dataPromise;
+      }).then(function (data) {
+        reviewPage(data);
+      })
+      .catch(function (error) {
+        alert("error");
+      })
+  }
+
+  $(".back").click(back);
+
+  function back() {
+    $("#overlay").addClass("hidden");
+  }
+
+  function reviewPage(info) {
+    var data = info[0];
+
+    // Picture
+    $("#review-image").attr("src", "img/" + data.info.src);
+    $("#review-image").attr("alt", data.info.alt);
+
+    // 1st Stars
+    var infoStarNumber = Math.floor(parseInt(data.info.averageRating));
+    $("#review-rating .fa").each(function (index) {
+      if (index < infoStarNumber) {
+        $(this).addClass("checked");
+      }
+    })
+
+    // Name
+    $("#review-name").html(data.info.foodName);
+
+    // Review description
+    $("#review-description").html(data.info.averageRating + " average based on " + data.info.ratingCount + " reviews.")
+
+    // Bars
+    $(".bar-5").css("width", data.info.ratings.fivestar + "%");
+    $(".bar-4").css("width", data.info.ratings.fourstar + "%");
+    $(".bar-3").css("width", data.info.ratings.threestar + "%");
+    $(".bar-2").css("width", data.info.ratings.twostar + "%");
+    $(".bar-1").css("width", data.info.ratings.onestar + "%");
+
+    // Reviews
+    var reviewSection = $("#review-review");
+    const reviewTemplate =
+      `<div id="review-reviews" class="review-row">
+          <div class="side">
+            <div>
+              <img src="img/placeholder.png" alt="profile picture" class="profile">
+              <p class="name">NAME</p>
+            </div>
+          </div>
+          <div class="middle">
+          <div class="mobile-head">
+            <p><img src="img/placeholder.png" alt="profile picture" class="mobile-profile">
+                NAME</p>
+            <div id="review-mobile-stars">
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+                <span class="fa fa-star"></span>
+            </div>
+          </div>
+        <p>REVIEW TEXT</p>
+
+      </div>
+      <div class="side right">
+          <div id="review-desktop-stars">
+            <span class="fa fa-star"></span>
+            <span class="fa fa-star"></span>
+            <span class="fa fa-star"></span>
+            <span class="fa fa-star"></span>
+            <span class="fa fa-star"></span>
+          </div>
+      </div>
+    </div>`;
+
+    $(reviewTemplate).appendTo(reviewSection);
+    $(reviewTemplate).appendTo(reviewSection);
+
+    //FINISH LATER
+  }
+
 });
